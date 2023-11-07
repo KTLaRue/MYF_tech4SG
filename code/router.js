605c58ae9668d89517eb107f1007578cf6f81eb4
Katie <laruek2@wwu.edu>
2023-11-07 14:10:38 -0800

    testing buttons - not working

--- code/router.js
+++ code/router.js
@@ -1,18 +1,36 @@
+
+
+// import test.js
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Home!')
-});
+// app.get('/', (req, res) => {
+//   res.send('Home!')
+// });
 
-app.get('/hello', (req, res) => {
+// app.get('/hello', (req, res) => {
     
-});
+// });
 
-app.get('*', (req, res) => {
-    res.send(':3')
-});
+// app.get('*', (req, res) => {
+//     res.send(':3')
+// });
+
+
+const myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+app.use(myLogger)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// app.listen(3000)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ code/test.js
@@ -0,0 +1,22 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-button");
+    const menuList = document.getElementById("menu-list");
+
+    menuButton.addEventListener("click", function () {
+        menuList.style.display = menuList.style.display === "block" ? "none" : "block";
+    });
+
+    const button1 = document.getElementById("button1");
+    const button2 = document.getElementById("button2");
+
+    button1.addEventListener("click", function () {
+        alert("Button 1 clicked!");
+    });
+
+    button2.addEventListener("click", function () {
+        alert("Button 2 clicked!");
+    });
+});
