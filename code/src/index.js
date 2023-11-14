6a7e298f6d5b7991b25fe84f92ab6e33ffa2d631
KTLaRue <laruek2@wwu.edu>
2023-11-14 13:06:46 -0800

    restructure

---
+++ code/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PostRequest from './EdamamTest';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<PostRequest />);
---
+++ code/test_folder/test.js
@@ -0,0 +1,24 @@
+const coolFunction= function (req, res, next){
+    console.log("button clicked")
+}
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const menuButton = document.getElementById("menu-button");
+//     const menuList = document.getElementById("menu-list");
+
+//     menuButton.addEventListener("click", function () {
+//         menuList.style.display = menuList.style.display === "block" ? "none" : "block";
+//     });
+
+//     const button1 = document.getElementById("button1");
+//     const button2 = document.getElementById("button2");
+
+//     button1.addEventListener("click", function () {
+//         alert("Button 1 clicked!");
+//     });
+
+//     button2.addEventListener("click", function () {
+//         alert("Button 2 clicked!");
+//     });
+// });
