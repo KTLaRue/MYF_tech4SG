4399b3b8326941d3f829e37c85c4c172a6451360
AnahitaSab <129370714+AnahitaSab@users.noreply.github.com>
2023-11-17 19:34:21 -0800

    Update app.js

--- code/MapTest1/app.js
+++ code/MapTest1/app.js
@@ -1,99 +1,97 @@
-function initMap(){
-
-    // Map option
-
+function initMap() {
     var options = {
-        center: {lat: 48.733843 , lng:-122.48647 },
+        center: { lat: 48.733843, lng: -122.48647 },
         zoom: 10
-    }
+    };
 
-    //New Map
-    map = new google.maps.Map(document.getElementById("map"),options)
+    var map = new google.maps.Map(document.getElementById("map"), options);
 
-    //listen for click on map location
-
-    google.maps.event.addListener(map, "click", (event) => {
-        //add Marker
-        addMarker({location:event.latLng});
-    })
-
-
-
-    //Marker
-    /*
-        const marker = new google.maps.Marker({
-        position:{lat: 37.9922, lng: -1.1307},
-        map:map,
-        icon:"https://img.icons8.com/nolan/2x/marker.png"
+    function addMarker(property) {
+        var marker = new google.maps.Marker({
+            position: property.location,
+            map: map,
         });
 
-        //InfoWindow
-
-        const detailWindow = new google.maps.InfoWindow({
-            content: `<h2>Murcia City</h2>`
-        });
-
-        marker.addListener("mouseover", () =>{
-            detailWindow.open(map, marker);
-        })
-        */
-
-    //Add Markers to Array
-
-    let MarkerArray = [ {location:{lat: 37.9922, lng: -1.1307},
-        imageIcon: "https://img.icons8.com/nolan/2x/marker.png",
-        content: `<h2>Murcia City</h2>`},
-
-        {location:{lat: 39.4699, lng: -0.3763}},
-
-        {location:{lat: 38.5411, lng: -0.1225},content: `<h2>Benidorm City</h2>` }
-
-
-
-    ]
-
-    // loop through marker
-    for (let i = 0; i < MarkerArray.length; i++){
-        addMarker(MarkerArray[i]);
-
-    }
-
-    // Add Marker
-
-    function addMarker(property){
-
-        const marker = new google.maps.Marker({
-            position:property.location,
-            map:map,
-            //icon: property.imageIcon
-        });
-
-        // Check for custom Icon
-
-        if(property.imageIcon){
-            // set image icon
-            marker.setIcon(property.imageIcon)
-        }
-
-        if(property.content){
-
-            const detailWindow = new google.maps.InfoWindow({
+        if (property.content) {
+            var infoWindow = new google.maps.InfoWindow({
                 content: property.content
             });
 
-            marker.addListener("mouseover", () =>{
-                detailWindow.open(map, marker);
-            })
+            marker.addListener('click', function () {
+                infoWindow.open(map, marker);
+            });
         }
-
-
-
-
-
     }
 
+    function autoFillUserLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                function (position) {
+                    const userLatLng = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
 
+                    map.setCenter(userLatLng);
 
+                    addMarker({
+                        location: userLatLng,
+                        content: "<h2>Your Location</h2>",
+                    });
+                },
+                function (error) {
+                    alert('Error getting your location: ' + error.message);
+                }
+            );
+        } else {
+            alert('Geolocation is not supported by your browser.');
+        }
+    }
 
+    function searchNearbyStores() {
+        const userLocationInput = document.getElementById('userLocation');
+        const userLocation = userLocationInput.value;
 
+        if (!userLocation) {
+            alert('Please enter a location or use your current location.');
+            return;
+        }
+
+        const geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ address: userLocation }, (results, status) => {
+            if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
+                const userLatLng = results[0].geometry.location;
+
+                const request = {
+                    location: userLatLng,
+                    radius: 5000,
+                    type: 'grocery_or_supermarket',
+                };
+
+                const service = new google.maps.places.PlacesService(map);
+
+                service.nearbySearch(request, (results, status) => {
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        // Clear existing markers
+                        // ...
+
+                        // Add markers for the nearby stores
+                        for (let i = 0; i < results.length; i++) {
+                            addMarker({
+                                location: results[i].geometry.location,
+                                content: `<h2>${results[i].name}</h2>`,
+                            });
+                        }
+                    } else {
+                        alert('Error searching for nearby stores: ' + status);
+                    }
+                });
+            } else {
+                alert('Error geocoding the location: ' + status);
+            }
+        });
+    }
+
+    document.getElementById('findStoresButton').addEventListener('click', searchNearbyStores);
+    document.getElementById('useMyLocationButton').addEventListener('click', autoFillUserLocation);
 }
