24890f2e641a2307d8ea3f759178c28a3b341100
kingj44 <91225196+kingj44@users.noreply.github.com>
2023-12-01 14:18:49 -0800

    Deleted code/test-react-app

--- code/test-react-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from '../../../test-react-app/src/RecipeSearch';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
