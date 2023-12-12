8ebf989d03c898f0fa7da947f86ca0e61ec2eccd
KTLaRue <laruek2@wwu.edu>
2023-12-12 15:46:12 -0800

    update text

--- working_front/src/components/foodBox.js
+++ working_front/src/components/foodBox.js
@@ -40,7 +40,7 @@ export function FoodBox({id, name, time, src, favorites, remove, url}) {
                   <a href={url}>Click Here For Recipe</a>
                 </span>
                 <button type="button" className="button">
-                  Add from Favorites
+                  Add to Favorites
                 </button>
               </div>
               <img
