4e73b935f69d1c0ad19fc0d2df193c354362ea53
AnahitaSab <129370714+AnahitaSab@users.noreply.github.com>
2023-12-12 14:19:32 -0800

    Update map_face.js

--- working_front/src/views/map_face.js
+++ working_front/src/views/map_face.js
@@ -1,158 +1,213 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Fragment } from "react";
-import "../styles/map_style.css"
-
-
+import React, { useEffect } from 'react';
+import { Fragment } from 'react';
+import '../styles/map_style.css';
 
 const Map_face = (props) => {
-    return (
-        <Fragment>
-            <div className="map-header">
-                <button onClick={() => goBack()}>Back</button>
-                <h1>Google Maps</h1>
-                {/* <!-- This is your button-link --> */}
-                {/* <a href="/path-to-your-destination" class="button-link">Your Label</a> */}
-            </div>
+  let map, directionsService, directionsRenderer, userLocation, selectedDestination;
+  const markers = [];
+  const defaultTravelMode = 'DRIVING';
 
-            <div id="map-container">
-                <div id="map"></div>
-                <div id="locationInput">
-                    {/* need to add on click event */}
-                    <button type="button" id="useMyLocationButton">Use My Location</button>
-                    <button type="button" id="getDirectionsButton">Get Directions</button>
-                </div>
-                <div id="directionsPanel"></div>
-            </div>
+  useEffect(() => {
+    window.initMap = initMap;
+    loadGoogleMapsScript();
 
-            {/*  <!-- Google Maps API script --> */}
-            <script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDSZjD7YEAuFV_jMfrSdcWmSYWUq9iTMJU&callback=initMap&libraries=places"></script>
+    return () => {
+      // Cleanup function if necessary
+    };
+  }, []);
 
-            <script>
-                {/* call use effects to render methods  - make sure to pass empty array */}
-                {/* https://www.w3schools.com/react/react_useeffect.asp */}
-                let map, directionsService, directionsRenderer, userLocation, selectedDestination;
-                const markers = [];
-                initMap();
-                autoFillUserLocation();
-                findNearbyStores(location);
-                {/* findNearbyStores(locationInput); */}
-                createMarker(place);
-                clearMarkers();
-                getDirections(origin, destination);
-                goBack();
-            </script>
-        </Fragment>
+  const loadGoogleMapsScript = () => {
+    const script = document.createElement('script');
+    script.src =
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyDSZjD7YEAuFV_jMfrSdcWmSYWUq9iTMJU&callback=initMap&libraries=places';
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  };
 
-    )
+  function initMap() {
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer();
 
+    const options = {
+      center: { lat: 48.769768, lng: -122.485886 },
+      zoom: 12,
+    };
 
-    function initMap() {
-        directionsService = new google.maps.DirectionsService();
-        directionsRenderer = new google.maps.DirectionsRenderer();
+    map = new google.maps.Map(document.getElementById('map'), options);
+    directionsRenderer.setMap(map);
 
-        const options = {
-            center: { lat: 48.733843, lng: -122.48647 },
-            zoom: 10
-        };
+    document.getElementById('useMyLocationButton').addEventListener('click', function () {
+      autoFillUserLocation();
+    });
 
-        map = new google.maps.Map(document.getElementById("map"), options);
-        directionsRenderer.setMap(map);
+    document.getElementById('getDirectionsButton').addEventListener('click', function () {
+      if (userLocation && selectedDestination) {
+        getDirections(userLocation, selectedDestination, defaultTravelMode);
+      } else {
+        alert('Please use your location and select a destination first.');
+      }
+    });
 
-        document.getElementById('useMyLocationButton').addEventListener('click', function () {
-            autoFillUserLocation();
-        });
+    // Call autoFillUserLocation here to ensure 'map' is defined
+    autoFillUserLocation();
+  }
 
-        document.getElementById('getDirectionsButton').addEventListener('click', function () {
-            if (userLocation && selectedDestination) {
-                getDirections(userLocation, selectedDestination);
-            } else {
-                alert('Please use your location and select a destination first.');
-            }
-        });
+  function autoFillUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          userLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          map.setCenter(userLocation);
+          findNearbyStores(userLocation);
+        },
+        function (error) {
+          if (error.code === error.PERMISSION_DENIED) {
+            alert('Geolocation permission denied. Please enable location services in your browser.');
+          } else {
+            alert('Error getting your location: ' + error.message);
+          }
+        },
+        { enableHighAccuracy: true }
+      );
+    } else {
+      alert('Geolocation is not supported by your browser.');
     }
+  }
 
-    function autoFillUserLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                function (position) {
-                    userLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                    map.setCenter(userLocation);
-                    findNearbyStores(userLocation);
-                },
-                function (error) {
-                    alert('Error getting your location: ' + error.message);
-                },
-                { enableHighAccuracy: true } // This is the added option for high accuracy
-            );
+  function findNearbyStores(location) {
+    const service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(
+      {
+        location: location,
+        radius: 5000,
+        type: ['grocery_or_supermarket'],
+      },
+      function (results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          clearMarkers();
+          for (let i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
         } else {
-            alert('Geolocation is not supported by your browser.');
+          alert('Failed to find grocery stores: ' + status);
         }
+      }
+    );
+  }
+
+  function createMarker(place) {
+    const marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location,
+      title: place.name,
+    });
+    markers.push(marker);
+
+    const infowindow = new google.maps.InfoWindow();
+    const contentString = `<div><strong>${place.name}</strong><br>${place.vicinity}</div>`;
+
+    marker.addListener('click', function () {
+      closeInfoWindow();
+      clearDirections();
+      infowindow.setContent(contentString);
+      infowindow.open(map, this);
+      selectedDestination = place.geometry.location;
+      map.currentInfoWindow = infowindow;
+    });
+  }
+
+  function closeInfoWindow() {
+    if (map.currentInfoWindow) {
+      map.currentInfoWindow.close();
+    }
+  }
+
+  function clearMarkers() {
+    for (let marker of markers) {
+      marker.setMap(null);
+    }
+    markers.length = 0;
+  }
+
+  function clearDirections() {
+    if (directionsRenderer) {
+      directionsRenderer.setDirections(null);
+      document.getElementById('directionsPanel').innerHTML = '';
+    }
+  }
+
+  function getDirections(origin, destination, travelMode) {
+    console.log('Selected Travel Mode:', travelMode);
+
+    const request = {
+      origin: origin,
+      destination: destination,
+      travelMode: google.maps.TravelMode[travelMode],
+    };
+
+    // Ensure proper initialization
+    if (!directionsRenderer) {
+      directionsRenderer = new google.maps.DirectionsRenderer();
+      directionsRenderer.setMap(map);
+    }
+    if (!directionsService) {
+      directionsService = new google.maps.DirectionsService();
     }
 
-    function findNearbyStores(location) {
-        const service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-            location: location,
-            radius: 5000,
-            type: ['store']
-        }, function(results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                clearMarkers();
-                for (let i = 0; i < results.length; i++) {
-                    createMarker(results[i]);
-                }
-            } else {
-                alert('Failed to find stores: ' + status);
-            }
-        });
+    // Clear directions and panel
+    directionsRenderer.setDirections(null);
+    document.getElementById('directionsPanel').innerHTML = '';
+
+    directionsService.route(request, function (response, status) {
+      if (status === 'OK') {
+        displayDirections(response, travelMode);
+      } else {
+        alert(`${travelMode} directions request failed: ${status}`);
+      }
+    });
+  }
+
+  function displayDirections(response, mode) {
+    if (directionsRenderer) {
+      directionsRenderer.setDirections(response);
+      document.getElementById('directionsPanel').innerHTML += `<h3>${mode} Directions</h3>`;
+      directionsRenderer.setPanel(document.getElementById('directionsPanel'));
+    } else {
+      alert('Directions renderer not initialized.');
     }
+  }
 
-    function createMarker(place) {
-        const marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location,
-            title: place.name
-        });
-        markers.push(marker);
+  function goBack() {
+    window.history.back();
+  }
 
-        const infowindow = new google.maps.InfoWindow();
-        marker.addListener('click', function() {
-            infowindow.setContent(place.name);
-            infowindow.open(map, this);
-            selectedDestination = place.geometry.location; // Set the clicked location as the destination
-        });
-    }
+  return (
+    <Fragment>
+      <div className="map-header">
+        <button onClick={goBack}>Back</button>
+        <h1>Google Maps</h1>
+      </div>
 
-    function clearMarkers() {
-        for (let marker of markers) {
-            marker.setMap(null);
-        }
-        markers.length = 0;
-    }
+      <div id="map-container">
+        <div id="map" className="map"></div>
+        <div id="locationInput">
+          <button type="button" id="useMyLocationButton">
+            Use My Location
+          </button>
 
-    function getDirections(origin, destination) {
-        const request = {
-            origin: origin,
-            destination: destination,
-            travelMode: google.maps.TravelMode.DRIVING
-        };
-
-        directionsService.route(request, function(response, status) {
-            if (status === 'OK') {
-                directionsRenderer.setDirections(response);
-                document.getElementById('directionsPanel').style.display = 'block';
-                directionsRenderer.setPanel(document.getElementById('directionsPanel'));
-            } else {
-                alert('Directions request failed: ' + status);
-            }
-        });
-    }
-
-    function goBack() {
-        window.history.back();
-    }
-
-}
+          <button
+            type="button"
+            id="getDirectionsButton"
+            onClick={() => getDirections(userLocation, selectedDestination, defaultTravelMode)}
+          >
+            Get Directions
+          </button>
+        </div>
+        <div id="directionsPanel"></div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default Map_face
